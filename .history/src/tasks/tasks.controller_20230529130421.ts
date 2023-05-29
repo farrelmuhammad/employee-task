089c3590b726fde98d9a1388ba80b8a31e58980b
Farrel Muhammad <farrelf11@gmail.com>
2023-05-29 15:48:21 +0700

    setting up task modle

---
+++ .history/src/tasks/tasks.controller_20230529130421.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { Get } from '@nestjs/common/decorators';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
+}
